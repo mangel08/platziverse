dac6c1917aebf0e1f94ebc3a962d41a56eb1e9ac
Miguelangel Palma <mangel.2711@gmail.com>
2018-05-12 17:28:25 -0400

    [implementando eventos en el servidor MQTT y manejando errores]

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -16,8 +16,36 @@ const settings = {
   backend
 }
 
+const handleFatalError = (err) => {
+  console.error(`${chalk.red('[fatal error]: ')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
 const server = new mosca.Server(settings)
 
+// Evento cuando el servidor MQTT esta corriendo
 server.on('ready', () => {
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
+
+// Evento de cliente conectado
+server.on('clientConnected', client => {
+  debug(`Client connected: ${client.id}`)
+})
+
+// Evento de cliente desconectado
+server.on('clientDisconnected', client => {
+  debug(`Client disconnected: ${client.id}`)
+})
+
+// Evento de captura de mensaje recibido por un cliente
+server.on('published', (packet, client) => {
+  debug(`Receive: ${packet.topic}`)
+  debug(`Receive: ${packet.payload}`)
+})
+
+server.on('error', handleFatalError)
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandleRejection', handleFatalError)
